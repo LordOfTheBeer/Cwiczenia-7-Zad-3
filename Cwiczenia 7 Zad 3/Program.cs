7780a75ca09a1874d3ad84ba4b0e4aee9535145d
LordOfTheBeer <116457854+LordOfTheBeer@users.noreply.github.com>
2023-01-28 16:44:44 +0100

    Add files via upload

---
+++ Cwiczenia 7 Zad 3/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Cwiczenia_7_Zad_3
+{
+   static class Funkcje
+   {
+        public static int Suma_do_n(int n)
+        {
+            int i, s = 0;
+            for (i = 1; i <= n; i++)
+                s += i;
+            return s;
+        }
+    }
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 20; i++)
+
+                Console.WriteLine(i + " " + Funkcje.Suma_do_n(i));
+        }
+    }
+}
